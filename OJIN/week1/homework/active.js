c780118486e27932b2ab70270e3f80442ddf99d6
권오진 <welsh-koj@MacBookPro50.local>
2026-03-24 00:59:58 +0900

    [Feat] 웹페이지 디자인 및 1주차 과제 완료

--- OJIN/week1/homework/active.js
+++ OJIN/week1/homework/active.js
@@ -5,9 +5,11 @@ const minus = document.getElementById("subBtn");
 //전체 숙소에 대한 가격 변동
 function updatePrice(amount) {
   stringPrice.forEach((price) => {
+    // 태그 안의 가격을 number로 인식. 숫자와 관련없는 문자 제거
     let currentPrice = Number(price.innerText.replace(/[^\d]/g, ""));
     let newPrice = currentPrice + amount;
 
+    // 숙소 가격이 음수가 될 수 없으므로 가격이 0보다 작아지면 0으로 출력
     if (newPrice < 0) newPrice = 0;
 
     price.innerText = `₩${newPrice.toLocaleString()}`;
