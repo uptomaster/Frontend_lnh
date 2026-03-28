a0b1677382c4277f6d500c776b56c300e0896399
youngyoom <tjdud9329@gmail.com>
2026-03-29 01:52:05 +0900

    [Add] '왕과 사는 남자' 영화 데이터 추가

--- seoyoung/week2/practice/movieList/src/pages/MovieList.jsx
+++ seoyoung/week2/practice/movieList/src/pages/MovieList.jsx
@@ -1,5 +1,26 @@
+import movieData from '../data/movie.json';
+
 const MovieList = () => {
-  return <div>MovieList 페이지 입니다.</div>;
+  const { movieImage, releaseDate, actor, title, director, description } =
+    movieData;
+
+  return (
+    <main className="p-10">
+      <section className="w-100 bg gray-800 p-6 rounded-lg shadow">
+        <img
+          src={movieImage}
+          alt={'${title} 영화 포스터'}
+          classNmae="w-full rounded mb-4"
+        />
+        <hw className="text-2xl font-semibold">{title}</hw>
+        <hr className="my-4" />
+        <p className="text-gray-200">주연배우 : {actor}</p>
+        <p className="text-gray-200">감독 : {director}</p>
+        <p className="text-gray-200">설명 : {description}</p>
+        <p className="text-gray-200 text-sm mt-2">개봉일 : {releaseDate}</p>
+      </section>
+    </main>
+  );
 };
 
 export default MovieList;
