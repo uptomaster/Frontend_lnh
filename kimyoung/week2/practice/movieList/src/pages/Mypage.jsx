76ecf88600f35403597f3a4070a908d74f3bc5c4
kimen <kmkang0619@gmail.com>
2026-03-30 08:49:26 +0900

    [FEAT] Mypage에 영화 추가

--- kimyoung/week2/practice/movieList/src/pages/Mypage.jsx
+++ kimyoung/week2/practice/movieList/src/pages/Mypage.jsx
@@ -1,5 +1,32 @@
-const MyPage = () => {
-  return <div>MyPage 페이지입니다.</div>;
+import movieData from '../data/myMovie.json';
+
+const Mypage = () => {
+  return (
+    <main className="p-10 flex gap-5">
+      {movieData.map((movie, index) => {
+        const { movieImage, releaseDate, actor, title, director, description } =
+          movie;
+
+        return (
+          <section className="w-100 bg-gray-800 p-6 rounded-lg shadow">
+            <img
+              src={movieImage}
+              alt={`${title} 영화 포스터`}
+              className="w-full rounded mb-4"
+            />
+            <h2 className="text-2xl font-semibold">{title}</h2>
+            <hr className="my-4" />
+            <p className="text-gray-200">🎭 주연배우: {actor}</p>
+            <p className="text-gray-200">🎬 감독: {director}</p>
+            <p className="text-gray-200">📝 설명: {description}</p>
+            <p className="text-gray-400 text-sm mt-2">
+              📅 개봉일: {releaseDate}
+            </p>
+          </section>
+        );
+      })}
+    </main>
+  );
 };
 
-export default MyPage;
+export default Mypage;
