86f236afb60df49e5dd4ded569835a2aa127fad0
안정규 <ahnjunggyu1114@gmail.com>
2026-04-06 20:05:22 +0900

    [FEAT] week2 넷플릭스 구현, 무비카드 추가

---
+++ junggyu/week2/pratice/src/pages/MovieCard.jsx
@@ -0,0 +1,19 @@
+const MovieCard = ({movie}) => {
+    const { movieImage, releaseDate, actor, title, director, description, link } = movie;
+
+    return(
+        <a href={link} target="_blank" rel="noreferrer" className="block">
+            <section className="w-100 bg-gray-800 p-6 rounded-lg shadow hover:-translate-y-2 hover:bg-gray-700">
+                <img src={movieImage} alt={`${title} 영화 포스터`} className="w-full rounded mb-4"/>
+                <h2 className="text-2xl font-semibold">{title}</h2>
+                <hr className="my-4" />
+                <p className="text-gray-200">🎭 주연배우: {actor}</p>
+                <p className="text-gray-200">🎬 감독: {director}</p>
+                <p className="text-gray-200">📝 설명: {description}</p>
+                <p className="text-gray-400 text-sm mt-2">📅 개봉일: {releaseDate}</p>
+            </section>
+        </a>
+    );
+};
+
+export default MovieCard;
