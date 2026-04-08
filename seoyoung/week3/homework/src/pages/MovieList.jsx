6b036ee5b465f356c6aee63a94ea7d9ed859333d
youngyoom <tjdud9329@gmail.com>
2026-04-08 11:17:27 +0900

    [Feat] week3 과제 환경 초기 설정

---
+++ seoyoung/week3/homework/src/pages/MovieList.jsx
@@ -0,0 +1,14 @@
+import MovieCard from '../components/MovieCard';
+import MovieData from '../data/movie.json';
+
+const MovieList = () => {
+  return (
+    <div className=" flex flex-row items-stretch">
+      {MovieData.map((movie) => (
+        <MovieCard key={movie.title} movie={movie} /> // 중요!!!
+      ))}
+    </div>
+  );
+};
+
+export default MovieList;
