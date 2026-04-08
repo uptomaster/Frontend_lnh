543193466c6c746008f7a1b59d4d2ada193c8883
youngyoom <tjdud9329@gmail.com>
2026-04-08 20:51:32 +0900

    [Add] 모달 레이아웃 구현 및 onClick 함수 적용

--- seoyoung/week3/homework/src/components/MovieModal.jsx
+++ seoyoung/week3/homework/src/components/MovieModal.jsx
@@ -1,20 +1,40 @@
-const MovieModal = ({ movie }) => {
+import './MovieModal.css';
+
+const MovieModal = ({ movie, onClose }) => {
   return (
-    <div>
-      <img
-        src={movie.movieImage}
-        alt={`${movie.title} 영화 포스터`}
-        className="aspect-[2/3] object-cover rounded mb-4" //이미지를 찌그러뜨리지 않음
-      />
-      <h2 className="m-2 text-2xl font-semibold hover:text-purple-300">
-        {movie.title}
-      </h2>
-      <hr className="my-4" />
-      <p className="text-gray-200">
-        <span className="font-bold">👩‍🎤 주연배우 : </span>
-        {movie.actor}
-      </p>
-      ;
+    <div
+      className="bg-black/80 p-4 z-50 flex items-center justify-center fixed inset-0"
+      onClick={onClose}
+    >
+      {/*fixed를 사용해서 그 위에 중첩되게 떠있는 화면효과 주기*/}
+      {/*inset-0 상화좌우 모든 방향의 여백을 0으로 만들기*/}
+      <div
+        className="relative bg-black p-8  max-w-md rounded-2xl"
+        onClick={
+          (e) => e.stopPropagation() //이 아래 자식 박스들은 onClick 신호가 가지 않음
+        }
+      >
+        <img
+          src={movie.movieImage}
+          alt={`${movie.title} 영화 포스터`}
+          className="w-full h-auto max-h-[65vh] rounded mb-4 aspect-[2/3] object-cover"
+        />
+        {/*화면 높이의 65%로 화면 높이 제한*/}
+        <button
+          onClick={onClose}
+          className="absolute text-white text-2xl top-1 right-2"
+        >
+          X
+        </button>
+        <h2 className="m-2 text-2xl font-semibold hover:text-purple-300">
+          {movie.title}
+        </h2>
+        <hr className="my-4" />
+        <p className="text-gray-200">
+          <span className="font-bold">👩‍🎤 주연배우 : </span>
+          {movie.actor}
+        </p>
+      </div>
     </div>
   );
 };
