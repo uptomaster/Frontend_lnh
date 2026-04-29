7ea840db84cb200ce9fcb5dd5284871b9c941ec0
youngyoom <tjdud9329@gmail.com>
2026-04-29 15:43:51 +0900

    [Feat] Mypage 기본 구조 생성

--- seoyoung/week4/homework/src/pages/MyPage.jsx
+++ seoyoung/week4/homework/src/pages/MyPage.jsx
@@ -1,13 +1,11 @@
+import {useState} from "react";
+import MovieModal from "../components/MovieModal";
+import MovieCard from "../components/MovieCard";
+import useRecentShows from "../hooks/useRecentShow";
+
 const Mypage = () => {
   return (
-    <main className="flex flex-row min-h-screen gap-6 text-white px-10 text-center">
-      <div className="w-100 m-4 p-4 pb-10 bg-gray-800 rounded-lg">
-        <h2 className="text-xl font-bold">✍️ 프로필</h2>
-      </div>
-      <div className="w-100 m-4 p-4 pb-10 bg-gray-800 rounded-lg">
-        <h2 className="text-xl font-bold">🎥 선호하는 영화 </h2>
-      </div>
-    </main>
+    const 
   );
 };
 
