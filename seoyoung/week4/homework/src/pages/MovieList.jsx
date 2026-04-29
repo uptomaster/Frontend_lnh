b67ee9e5bfca7c5b984da6e8db086d00419695ad
youngyoom <tjdud9329@gmail.com>
2026-04-29 12:04:04 +0900

    [ADD] shows, query, searchResult, selectedShow 상태 추가

--- seoyoung/week4/homework/src/pages/MovieList.jsx
+++ seoyoung/week4/homework/src/pages/MovieList.jsx
@@ -1,7 +1,21 @@
+import axios from 'axios';
 import MovieCard from '../components/MovieCard';
 import MovieData from '../data/movie.json';
+import { useState } from 'react';
 
 const MovieList = () => {
+  //TV show 목록 저장
+  const [shows, setShows] = useState([]);
+
+  //검색창에 입력한 값 저장
+  const [query, setQuery] = useState('');
+
+  //검색 결과를 저장
+  const [searchResult, setSearchResult] = useState([]);
+
+  //클릭한 영화 정보를 저장
+  const [selectedShow, setSelectedShow] = useState(null);
+
   return (
     <div className=" flex flex-row items-stretch">
       {MovieData.map((movie) => (
