f77622f1f337aa721e71c028977050cabf3de479
youngyoom <tjdud9329@gmail.com>
2026-04-29 12:43:16 +0900

    [Add] useEffect로 영화 데이터 API 추가

--- seoyoung/week4/homework/src/pages/MovieList.jsx
+++ seoyoung/week4/homework/src/pages/MovieList.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import MovieCard from '../components/MovieCard';
 import MovieData from '../data/movie.json';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import useRecentShows from '../hooks/useRecentShow';
 
 const MovieList = () => {
   //TV show 목록 저장
@@ -15,6 +16,26 @@ const MovieList = () => {
 
   //클릭한 영화 정보를 저장
   const [selectedShow, setSelectedShow] = useState(null);
+
+  //최근 본 영화 추가해주는 함수
+  const { addshow } = useRecentShows();
+
+  //처음 페이지가 열릴 때, 전체 show의 목록들을 가져와야됨
+  useEffect(() => {
+    //요청을 취소하기위한 컨트롤러
+    const controller = new AbortController();
+
+    axios
+      //서버에서 영화를 가져와서, {기존요청 취소해주는 컨트롤러}
+      .get('http://api.tvmaze.com/shows', { signal: controller.signal })
+      //요청이 성공하면 res : 서버의 응답 res.data : 실제 서버가 보내준 데이터 <- res.data를 shows에 저장
+      .then((res) => setShows(res.data))
+      //실패하면 실행
+      .catch(() => []);
+
+    //컴포넌트가 사라지면 요청 취소
+    return () => controller.abort();
+  });
 
   return (
     <div className=" flex flex-row items-stretch">
