2d75510a82824b88b09f9cdbf5b80a35a867130d
youngyoom <tjdud9329@gmail.com>
2026-04-29 11:20:04 +0900

    [Feat] week4 과제 초기 환경 구현

---
+++ seoyoung/week4/homework/src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Mypage from './pages/MyPage';
+import NotFound from './pages/NotFound';
+import Top100 from './pages/Top100';
+import MovieList from './pages/MovieList';
+import { Layout } from './components/layouts/Layout';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MovieList />} />
+          <Route path="/Mypage" element={<Mypage />} />
+          <Route path="/Top100" element={<Top100 />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+//return <div className="text-red-400 hover:text-red-600">app</div>;
+//속성(text=글자색상)-색상계열-밝기(500이 기본)
---
+++ seoyoung/week4/homework/src/pages/Top100.jsx
@@ -0,0 +1,11 @@
+const Top100 = () => {
+  return (
+    <div>
+      <h1 className="pt-10 text-2xl text-center font-bold ">
+        오늘 대한민국의 Top 100 영화
+      </h1>
+    </div>
+  );
+};
+
+export default Top100;
