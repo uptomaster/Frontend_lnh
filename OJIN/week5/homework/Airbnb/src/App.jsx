e2a7f97244e9844bf654a7b143ac5dfa4413ddd7
KwonOjin <ojin630@naver.com>
2026-05-04 11:02:54 +0900

    [FEAT] 컴포넌트 초기 설정

--- OJIN/week5/homework/Airbnb/src/App.jsx
+++ OJIN/week5/homework/Airbnb/src/App.jsx
@@ -1,5 +1,19 @@
+import NavBar from "@/components/NavBar";
+import SearchSection from "@/components/SearchSection";
+import InfoSection from "@/components/InfoSection";
+import StaySection from "@/components/StaySection";
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <NavBar />
+      <main>
+        <SearchSection />
+        <InfoSection />
+        <StaySection />
+      </main>
+    </div>
+  );
 }
 
 export default App;
