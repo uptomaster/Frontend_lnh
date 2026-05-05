6b1e584a4931d4e438bd016843416a3dca8732c1
youngyoom <tjdud9329@gmail.com>
2026-05-06 01:32:03 +0900

    [Mod] 관련 없는 파일들 삭제

--- seoyoung/week5/homework2/Airbnb/src/components/PlaceCard - 복사본.jsx
+++
@@ -1,5 +0,0 @@
-const PlaceCard = () => {
-  return <div>숙소 Card</div>;
-};
-
-export default PlaceCard;
--- seoyoung/week5/homework2/Airbnb/src/components/SearchCard - 복사본.jsx
+++
@@ -1,5 +0,0 @@
-const SearchCard = () => {
-  return <div>서울 숙소기 </div>;
-};
-
-export default SearchCard;
