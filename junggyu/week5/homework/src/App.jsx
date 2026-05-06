b887eaeddc3e22845942180efccb001ece2f97bd
안정규 <ahnjunggyu1114@gmail.com>
2026-05-07 03:29:15 +0900

    [FEAT] week05 과제, navbar 및 search 구현

--- junggyu/week5/homework/src/App.jsx
+++ junggyu/week5/homework/src/App.jsx
@@ -5,13 +5,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import './index.css'
+import Layout from "./components/layout/layout";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>} />
+        </Route>
         <Route path="*" element={<NotFound/>} />
       </Routes>    
     </BrowserRouter>
--- junggyu/week5/homework/src/pages/Home.jsx
+++ junggyu/week5/homework/src/pages/Home.jsx
@@ -3,26 +3,33 @@ import infoList from '../data/InformationCardList';
 import HotelCard from "../components/HotelCard";
 import hotelList1 from '../data/HotelList01';
 import hotelList2 from '../data/HotelList02';
+import Search from '../components/Search';
+import Img from '../assets/hongik.jpg';
 
 const Home = () => {
   return (
     <div>
-      <div>
-        레이아웃 / 네브바
-      </div>
 
       <main>
-        <section>
-          서울 숙소 찾기 영역
+        <section className="relative pt-[80px] pl-[80px] pr-[80px] ">
+          <div className="w-[70%] ml-auto">
+            <img
+              src={Img}
+              className=" max-w-[1400px] aspect-[2/1]  object-cover rounded-[12px]"
+            />
+          </div>
+          <div className=" absolute left-[80px] top-[120px] block w-[420px] rounded-[24px] ">
+          <Search />
+          </div>
         </section>
 
-        <section className="flex gap-[48px] justify-center pl-[80px] pr-[80px]">
+        <section className="flex gap-[48px] justify-center pt-[80px] pl-[80px] pr-[80px]">
           {infoList.map((info) => (
             <InfoCard image={info.image} title={info.title} desc={info.desc} />
           ))}
         </section>
 
-        <section className="pl-[80px] pr-[80px]">
+        <section className="pt-[64px] pl-[80px] pr-[80px]">
           <h3 className="text-[32px]">서울의 게스트 선호 숙소</h3>
           <p className="text-[18px]">평점, 후기, 신뢰도를 바탕으로 에어비앤비에서 가장 사랑받는 숙소로 손꼽히는 곳입니다.</p>
           <div className="flex gap-[24px] justify-center">
@@ -40,7 +47,7 @@ const Home = () => {
           </div>
         </section>
 
-        <section className="pl-[80px] pr-[80px]">
+        <section className="pt-[64px] pl-[80px] pr-[80px]">
           <h3 className="text-[32px]">넓은 공간을 갖춘 숙소</h3>
           <p className="text-[18px]">꿈에 그리던 휴가를 위한 편안한 독채 숙소를 찾아보세요.</p>
           <div className="flex gap-[24px] justify-center">
@@ -58,8 +65,8 @@ const Home = () => {
           </div>
         </section>
 
-        <section>
-          자주 묻는 질문과 답변 영역
+        <section className="py-[32px] pl-[80px] pr-[80px]">
+          자주 묻는 질문과 답변 영역 // 미구현
           
         </section>
       </main>
