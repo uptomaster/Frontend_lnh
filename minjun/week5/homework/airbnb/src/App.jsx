c7a14f537e95d7c367a16e25ba4103b4e8dcad13
minjun <mrainbeat@gmail.com>
2026-05-06 20:25:25 +0900

    [Feat] 에어비앤비 react 구현

---
+++ minjun/week5/homework/airbnb/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "./components/layouts/Layout";
+import HouseCard from "./components/HouseCard";
+import "./App.css";
+
+function App() {
+  return (
+    <Layout>
+      <HouseCard />
+    </Layout>
+  );
+}
+
+export default App;
