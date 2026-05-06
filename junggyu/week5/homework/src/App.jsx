8945bd2d3601400f5715102493919b3510b7e469
안정규 <ahnjunggyu1114@gmail.com>
2026-05-06 11:32:14 +0900

    [FEAT] week05 과제, 기본틀 생성

---
+++ junggyu/week5/homework/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import './index.css'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>    
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ junggyu/week5/homework/src/pages/Home.jsx
@@ -0,0 +1,40 @@
+const Home = () => {
+  return (
+    <div>
+      <div>
+        레이아웃 / 네브바
+      </div>
+
+      <main>
+        <section>
+          서울 숙소 찾기 영역
+        </section>
+
+        <section>
+          정보 Card 영역
+        </section>
+
+        <section>
+          서울의 게스트 선호 숙소 영역
+          
+        </section>
+
+        <section>
+          넓은 공간을 갖춘 숙소 영역
+          
+        </section>
+
+        <section>
+          자주 묻는 질문과 답변 영역
+          
+        </section>
+      </main>
+
+      <footer>
+        푸터 영역
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
