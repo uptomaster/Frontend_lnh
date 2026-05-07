f52353af61d714449433d50539cf02883c287857
kimen <kmkang0619@gmail.com>
2026-05-07 14:05:31 +0900

    [Feat] 레이아웃 및 네비게이션 구현

--- kimyoung/week5/homework/src/App.jsx
+++ kimyoung/week5/homework/src/App.jsx
@@ -1,8 +1,10 @@
+import Layout from "./components/Layout";
+
 function App() {
   return (
-    <div>
-      <h1>Airbnb Homework</h1>
-    </div>
+    <Layout>
+      <h1>서울 숙소 찾기</h1>
+    </Layout>
   );
 }
 
