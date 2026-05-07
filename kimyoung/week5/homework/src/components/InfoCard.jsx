3f88c8e3112b1bc249d6ef70b14687dafbc52c32
kimen <kmkang0619@gmail.com>
2026-05-07 14:09:49 +0900

    [Feat] 숙소 카드 리스트 렌더링 구현

---
+++ kimyoung/week5/homework/src/components/InfoCard.jsx
@@ -0,0 +1,11 @@
+function InfoCard({ icon, title, text }) {
+  return (
+    <div className="info-card">
+      <div className="info-icon">{icon}</div>
+      <h3>{title}</h3>
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default InfoCard;
