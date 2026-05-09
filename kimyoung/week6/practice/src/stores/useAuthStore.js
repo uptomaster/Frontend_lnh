f5536631819b83d8ceea8ba9976d3eed3bec6e1b
kimen <kmkang0619@gmail.com>
2026-05-09 21:17:14 +0900

    [Feat] 로그인 기능 구현

---
+++ kimyoung/week6/practice/src/stores/useAuthStore.js
@@ -0,0 +1,18 @@
+import { create } from 'zustand'; // zustand 상태관리 라이브러리
+import { persist } from 'zustand/middleware'; // zustand 상태 영속화 미들웨어
+const useAuthStore = create(
+  persist(
+    (set, get) => ({
+      accessToken: null, // 초기 accessToken 상태 = null
+
+      //토큰 저장 함수
+      setAccessToken: (token) => set({ accessToken: token }),
+
+      // 로그인 여부 확인. accessToken이 존재하면 true 반환
+      isloggedIn: () => !!get().accessToken,
+    }),
+    { name: 'auth-storage' }
+  )
+);
+
+export default useAuthStore;
