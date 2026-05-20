10bbc12277a78a1e6d09a7ec6edf44e435b8eb94
Minseo_Joh <whalstj9827@naver.com>
2026-05-21 02:15:42 +0900

    [Feat]App.jsx 경로

--- minseo/week6/homework/movielist/src/App.jsx
+++ minseo/week6/homework/movielist/src/App.jsx
@@ -4,6 +4,8 @@ import NotFound from "./pages/NotFound"
 import Top100 from "./pages/Top100"
 import MovieList from "./pages/MovieList"
 import Login from "./pages/Login"
+// ✨ [추가] 회원가입 컴포넌트 임포트
+import Signup from "./pages/Signup" 
 import Layout from "./components/layouts/Layout"
 import useAuthStore from './stores/useAuthStore'
 
@@ -16,6 +18,8 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<MovieList />} />
           <Route path="login" element={<Login />} />
+          {/* ✨ [추가] 회원가입 라우트 연결 */}
+          <Route path="signup" element={<Signup />} />
           <Route path="mypage" element={accessToken ? <Mypage /> : <Navigate to='/login' replace/>} />
           <Route path="top100" element={<Top100 />} />
           <Route path="*" element={<NotFound />} />
