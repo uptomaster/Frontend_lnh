b691d00ff4b45e1370858dc10740ae5daf2f3877
KevinHan142 <131000805+KevinHan142@users.noreply.github.com>
2026-05-21 18:43:37 +0900

    FIX : auth 오류 수정

--- jeonghoo/week6/practice/netflix/src/apis/authApi.js
+++ jeonghoo/week6/practice/netflix/src/apis/authApi.js
@@ -2,28 +2,40 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export const loginAPI = async ({
-  username,
-  password,
-}) => {
-  try {
-    const { data } = await axios.post(
-      `${BASE_URL}/api/auth/login`,
-      {
-        username,
-        password,
-      }
-    );
-
-    const accessToken = data.accessToken;
-
-    if (!accessToken) {
-      throw new Error("로그인 실패");
+export const loginAPI = async ({ username, password }) => {
+  const { data } = await axios.post(
+    `${BASE_URL}/api/auth/login`,
+    {
+      username,
+      password,
     }
+  );
 
-    return accessToken;
-  } catch (error) {
-    console.log(error.response?.data);
-    throw error;
-  }
+  return data.accessToken;
+};
+
+export const signupAPI = async ({ email, password }) => {
+  const { data } = await axios.post(
+    `${BASE_URL}/api/auth/signup`,
+    {
+      email,
+      password,
+    }
+  );
+
+  return data;
+};
+
+export const logoutAPI = async (accessToken) => {
+  const { data } = await axios.post(
+    `${BASE_URL}/api/auth/logout`,
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
+
+  return data;
 };
